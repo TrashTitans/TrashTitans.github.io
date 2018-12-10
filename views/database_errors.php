539a061d9ba0327145db257c9bddb58029b069ee
TrashTitans <45584878+TrashTitans@users.noreply.github.com>
2018-12-10 01:54:03 -0500

    Add files via upload

---
+++ views/database_errors.php
@@ -0,0 +1,19 @@
+<?php include 'header.php';?>
+
+<body>
+	<div id="header"
+	<h1>CISY 5303 Students</h1>
+	</div>
+	
+	<div id="main">
+		<h3>Database Error</h3>
+		<p>Soon we will have some generic error message.</p>
+		
+		<p>Error Message: <?php echo $error; ?>
+		<h2><a href="../index.php"> View Student List </a></h2>
+	</div>
+	
+	</body>
+
+<?php include 'footer.php';?>
+
